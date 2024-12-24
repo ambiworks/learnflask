4875866abcc633fa63806397846bb78e21718110
Claudio <claudrguez@gmail.com>
2024-12-23 22:02:51 -0800

    Add descriptive info

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
 from flask import Flask, url_for
 import serial.tools.list_ports
 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
 app = Flask(__name__)
 
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
 @app.route('/')
+# ‘/’ URL is bound with index() function.
 def index():
     return 'index'
 
@@ -22,3 +30,9 @@ def port_select():
     for port, description, hardware_id in ports:
         devices += f"Port: {port}, Description: {description}\n"
     return f'{devices}'
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
